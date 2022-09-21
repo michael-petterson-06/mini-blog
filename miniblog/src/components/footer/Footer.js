f7efa1fcdb23c4b0e9841ea12ea0407ed3a340c1
Michael <michaelpetterson06.com.br>
2022-09-21 11:53:09 -0300

    Adicionando componente de navbar e footer

---
+++ miniblog/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.css';
+// import styles from './NavBar.module.css';
+
+const Footer = () => {
+  return (
+    <footer>
+        <h3>Escreva sobre seus interesses!</h3>
+        <p>Mini Glog &copy; 2022</p>
+    </footer>
+  )
+}
+
+export default Footer
