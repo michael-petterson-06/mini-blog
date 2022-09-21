f7d597c6b9bc3bddacfc72923e78047acf7a8ba7
Michael <michaelpetterson06.com.br>
2022-09-21 13:30:02 -0300

    CSS NavBar e Footer

--- miniblog/src/components/navbar/NavBar.js
+++ miniblog/src/components/navbar/NavBar.js
@@ -4,19 +4,19 @@ import styles from './NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <nav>
-        <NavLink to='/'>
+    <nav className={ styles.navbar}>
+        <NavLink to='/' className={ styles.brand}>
             Mini <span>Blog</span>
         </NavLink>
-        <ul>
+        <ul className={ styles.links_list}>
             <li>
-                <NavLink to='/'>
+                <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : "")}>
                     Home
                 </NavLink>
             </li>
             <li>
-                <NavLink to='/about'>
-                    Sobre
+                <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : "")}>
+                    Sobre            
                 </NavLink>
             </li>
         </ul> 
